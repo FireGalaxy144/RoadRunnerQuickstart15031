0dfa7037e5a15265f5af1993f962e1c38a11a40e
FireGalaxy144 <89485180+FireGalaxy144@users.noreply.github.com>
2024-11-27 10:38:56 -0600

    Update Red_Long.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Red_Long.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Red_Long.java
@@ -1,5 +1,4 @@
-package org.firstinspires.ftc.teamcode.Auto;
-import androidx.annotation.NonNull;
+package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ParallelAction;
@@ -14,52 +13,96 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+import org.firstinspires.ftc.teamcode.roadrunner.SparkFunOTOSDrive;
+import org.firstinspires.ftc.teamcode.subsystems.arm;
+import org.firstinspires.ftc.teamcode.subsystems.claw;
+import org.firstinspires.ftc.teamcode.subsystems.drive;
+import org.firstinspires.ftc.teamcode.subsystems.lift;
+import org.firstinspires.ftc.teamcode.subsystems.wrist;
+import org.firstinspires.ftc.teamcode.util.MercurialAction;
+import org.firstinspires.ftc.teamcode.util.SilkRoad;
+import org.firstinspires.ftc.teamcode.util.WaitAction;
 
-// Non-RR imports
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import dev.frozenmilk.dairy.core.util.controller.calculation.pid.DoubleComponent;
+import dev.frozenmilk.dairy.core.util.features.BulkRead;
+import dev.frozenmilk.mercurial.Mercurial;
+import dev.frozenmilk.mercurial.commands.groups.Parallel;
+import dev.frozenmilk.mercurial.commands.util.Wait;
 
-import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-
-
-@Autonomous(name="RedShort", group="Robot")
-
-
-public class    Red_Short extends LinearOpMode {
-
-    private DcMotorEx wheelFL;
-    private DcMotorEx wheelFR;
-    private DcMotorEx wheelBL;
-    private DcMotorEx wheelBR;
-    private  Servo Claw;
-    private ElapsedTime runtime = new ElapsedTime();
-
-
-    static final double FORWARD_SPEED = 0.6;
-    static final double TURN_SPEED = 0.5;
+@Autonomous(name = "2 sample")
+@SilkRoad.Attach
+@Mercurial.Attach
+@lift.Attach
+@claw.Attach
+@wrist.Attach
+@arm.Attach
+@BulkRead.Attach
+@drive.Attach
+public class Auto extends OpMode {
+    private final Pose2d initialPose = new Pose2d(-24, -62.5, Math.toRadians(180));
+    private Action path0, path1, path2, path3;
+    private SparkFunOTOSDrive drive;
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+    public void init() { drive = new SparkFunOTOSDrive(hardwareMap, initialPose); }
 
-        // We want to start the bot at x: 10, y: -8, heading: 90 degrees
-        Pose2d startPose = new Pose2d(10, -8, Math.toRadians(90));
+    @Override
+    public void init_loop(){
 
-        drive.setPoseEstimate(startPose);
+        TrajectoryActionBuilder tab0 = drive.actionBuilder(initialPose)
+                .splineTo(new Vector2d(-50, -58), Math.toRadians(225));
+        TrajectoryActionBuilder tab1 = drive.actionBuilder(new Pose2d(-50, -58, Math.toRadians(225)))
+                .setReversed(true)
+                .splineTo(new Vector2d(-34, -40), Math.toRadians(0))
+                .setReversed(false)
+                .splineTo(new Vector2d( -46, -60), Math.toRadians(90))
+                .lineToY(62);
+        TrajectoryActionBuilder tab2 = drive.actionBuilder(new Pose2d(-46, -62, Math.toRadians(90)))
+                .setReversed(true)
+                .splineTo(new Vector2d(-34, -48), Math.toRadians(0))
+                .setReversed(false)
+                .splineTo(new Vector2d(-58, -49), Math.toRadians(225));
+        TrajectoryActionBuilder tab3 = drive.actionBuilder(new Pose2d(-58, -49, Math.toRadians(225)))
+                .setReversed(true)
+                .splineTo(new Vector2d(-34, -48), Math.toRadians(0))
+                .setReversed(false)
+                .splineTo(new Vector2d(-30, -34), Math.toRadians(0));
+        path0 = tab0.build();
+        path1 = tab1.build();
+        path2 = tab2.build();
+        path3 = tab3.build();
+    }
 
-        Trajectory traj1 = drive.trajectoryBuilder(startPose)
-                .splineTo(new Vector2d(20, 9), Math.toRadians(45))
-                .build();
+    @Override
+    public void start(){
+        SilkRoad.RunAsync(
+                new SequentialAction(
+                        new MercurialAction(new Parallel(lift.goTo(4500), wrist.wristFlat(), claw.closeClaw(), arm.armUp())),
+                        path0,
+                        new MercurialAction(claw.openClaw()),
+                        new SleepAction(1),
+                        new MercurialAction(new Parallel(claw.closeClaw(), lift.goTo(0), wrist.wristFlat(), arm.armStow())),
+                        path1,
+                        new MercurialAction(new Parallel(lift.goTo(0), arm.armOut(), claw.openClaw())),
+                        new SleepAction(2),
+                        new MercurialAction(wrist.wristDown()),
+                        new SleepAction(1),
+                        new MercurialAction(claw.closeClaw()),
+                        new SleepAction(1),
+                        new MercurialAction(new Parallel(arm.armUp(), wrist.wristFlat(), lift.goTo(4500))),
+                        path2,
+                        new MercurialAction(claw.openClaw()),
+                        new SleepAction(1),
+                        new MercurialAction(new Parallel(arm.armStow(), wrist.wristDown(), claw.closeClaw(), lift.goTo(0))),
+                        path3,
+                        new SleepAction(1),
+                        new MercurialAction(new Parallel(arm.armUp(), wrist.wristFlat()))
+                )
+        );
+    }
 
-        Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
-                .splineTo(new Vector2d(20, 9), Math.toRadians(45))
-                .build();
-
-        drive.followTrajectory(traj1);
-        drive.followTrajectory(traj2);
+    @Override
+    public void loop() {
     }
 }
